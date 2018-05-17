9409e0c744a4fd4f3e848337785ce0adddb58a75
caoanlong <476977540@qq.com>
2018-05-17 08:37:05 +0800

    gggg

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const Koa = require('koa')
 const logger = require('koa-logger')
-const bodyParser = require('koa-bodyparser')
+const koaBodyparser = require('koa-bodyparser')
 
 const router = require('./routers')
 
 const app = new Koa()
 app.use(logger())
-app.use(bodyParser())
+
 
 app.use(async function(ctx, next) {
 	ctx.set("Access-Control-Allow-Origin", "*")
 	// ctx.set("Access-Control-Allow-Origin", ctx.request.header.origin)
 	// ctx.set("Access-Control-Allow-Credentials", true)
 	// ctx.set("Access-Control-Max-Age", 86400000)
-	// ctx.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE")
-	// ctx.set("Access-Control-Allow-Headers", "x-requested-with, accept, origin, content-type")
+	ctx.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE")
+	ctx.set("Access-Control-Allow-Headers", "Content-Type, Accept, X-Access-Token")
 	await next()
 })
-
+// app.use(koaBodyparser())
 app.use(router.routes())
 
 module.exports = app
